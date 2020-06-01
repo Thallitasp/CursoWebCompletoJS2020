67ad37e9898d49388ecae081c2611ad1554e5dfd
Thallitasp <thallitaspedreira@gmail.com>
2020-06-01 00:00:32 -0300

    aulas19-30

---
+++ Fundamentos/objeto.js
@@ -0,0 +1,23 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Moderno'
+prod1.preco = 499.99
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 35.99
+}
+
+console.log(prod2)
+
+const prod3 = {
+    nome: 'Camisa',
+    preco: 5.99,
+    obj:{
+        bla: 1,
+        obj:{
+            blabla = 2
+        }
+    }
+}
---
+++ Fundamentos/usandoVar1.js
@@ -0,0 +1,9 @@
+{{{{var sera = "Será???"}}}}
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+}
+
+//console.log(local) variavel dentro de funcao só é vista na funcao
